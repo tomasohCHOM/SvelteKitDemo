777cc4f88f0f6465dd8432dee130533bfc3d796e
emilyytsai <emilytsai@csu.fullerton.edu>
2025-02-27 17:13:23 -0800

    done

--- src/routes/emily-daisy-dianella/+page.svelte
+++ src/routes/emily-daisy-dianella/+page.svelte
@@ -2,6 +2,7 @@
 <div>
     <p>Click the Cookie!</p>
     <p>Cookie Count: {count}</p>
+    <p> Milk Count: {count}</p>
 </div>
 
 <button 
@@ -9,13 +10,26 @@
     class="cookie-button">
     <img 
         alt="Perfect Cookie" 
-        src="https://cookieclicker.wiki.gg/images/thumb/5/5a/PerfectCookie.png/210px-PerfectCookie.png" 
+        src="https://i.pinimg.com/736x/dc/4d/21/dc4d21953049b5f11687ce7f681c583c.jpg"
         class="cookie-image"
     />
+
+</button>
+
+<button
+    on:click={() => count++} 
+    class="milk-button">
+    <img 
+        alt="Cute Milk" 
+        src="https://i.pinimg.com/736x/b6/fc/fb/b6fcfb02627e6ab2d6ad33e2ddaef2b7.jpg"
+        class="milk-image"
+    />
+
 </button>
 
 <script>
     let count = 0;
+    let mulltiplier = 1;
 </script>
 
 <style>
