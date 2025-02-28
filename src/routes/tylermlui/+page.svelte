9e99b23bb64561301fed2765d5ea92914758754f
Kevin-dgc <kevingeierconney@gmail.com>
2025-02-27 17:07:36 -0800

    commit w timmy!

--- src/routes/tylermlui/+page.svelte
+++ src/routes/tylermlui/+page.svelte
@@ -1,21 +1,23 @@
-<h1>Welcome to Cookie Clicker</h1>
+<h1>Welcome to Kookie Clicker</h1>
 <div>
-    <p>Click the Cookie!</p>
-    <p>Cookie Count: {count}</p>
+    <p>Click the Kookie!</p>
+    <p>Kookie Count: {count}</p>
 </div>
 
 <button 
     on:click={() => count++} 
+    <div style="background-color: {backgroundColor}; height: 100vh;">
     class="cookie-button">
     <img 
         alt="Perfect Cookie" 
-        src="https://cookieclicker.wiki.gg/images/thumb/5/5a/PerfectCookie.png/210px-PerfectCookie.png" 
-        class="cookie-image"
+        src="https://ip.prod.freshop.retail.ncrcloud.com/resize?url=https://images.freshop.ncrcloud.com/1564405684712279574/1bc4c860ff322d9a08a73a29c8e04e25_large.png&width=512&type=webp&quality=90" 
+        class="cookie_img"
     />
 </button>
 
 <script>
     let count = 0;
+    let backgroundColor ='skyblue';
 </script>
 
 <style>
@@ -25,4 +27,11 @@
         padding: 0;
         cursor: pointer; 
     }
+    .cookie_img{
+        width: 100px;
+        height: 100px;
+        border: none;
+        background: none;
+    }
+
 </style>
